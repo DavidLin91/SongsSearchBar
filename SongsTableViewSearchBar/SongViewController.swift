41d91530309e1bb3c4549fea3f5d8e8d79848956
David <lin.david985@gmail.com>
2019-12-06 16:29:26 -0500

    working on search query (stuck on how to switch between the two cases).

--- SongsTableViewSearchBar/SongViewController.swift
+++ SongsTableViewSearchBar/SongViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-enum CurrentScope {
+enum SearchScope {
     case songName
     case artistName
 }
@@ -23,27 +23,42 @@ class SongViewController: UIViewController {
         }
     }
     
+    var currentScope = SearchScope.songName
+    
+    var searchQuery = "" {
+        didSet {
+            switch currentScope {
+            case .songName:
+                Song.loveSongs = 
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        searchBar.delegate = self
         loadData()
     }
     
-    func loadData(){
+    func loadData() {
         songs = Song.loveSongs
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let song = segue.destination as? SongsDetailedViewController, let indexPath = tableView.indexPathForSelectedRow else {
-            fatalError()
+        guard let songDVC = segue.destination as? SongsDetailedViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {
+                fatalError()
         }
-        song.songDetails = songs[indexPath.row]
+        let song = songs[indexPath.row]
+        songDVC.songDetails = song
     }
 }
 
 extension SongViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return songs.count
+        return songs.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "songNameCell", for: indexPath)
@@ -51,5 +66,33 @@ extension SongViewController: UITableViewDataSource {
         cell.textLabel?.text = songName.name
         cell.detailTextLabel?.text = songName.artist
         return cell
+    }
+}
+
+extension SongViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            loadData()
+            return
+        }
+        searchQuery = searchText
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        print("selectedScope: \(selectedScope)")
+        switch selectedScope {
+        case 0:
+            currentScope = .songName
+            break
+        case 1:
+            currentScope = .artistName
+            break
+        default:
+            print("not a valid scope")
+        }
     }
 }
